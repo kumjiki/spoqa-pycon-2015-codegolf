1d091a881108ccecf564920ce03118656f19b9b1
Young-Hui Je <younghui.je@ideaware.co.kr>
2015-08-30 16:42:09 +0900

    using ascii as position data

--- pupu.py
+++ pupu.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
-import zlib, binascii
-
-def draw_pupu():
-    print(zlib.decompress(binascii.a2b_base64("eJzdlDsWgCAMBPucgjr3P6CFPM1nDSShkgrXzDyCCI384HtQmSzB3ICZj9EzaEnO9dLqq6WS+9Fz6b2NbLFKla66LKkyoq/tEO9W/8OXYrGAPcyBu5hHM6Rhc6iGs6yE02yXFriKvQ37ET4fROSCEDeTYQOEg5rhAsuH+JyihR3GYckSfwWMilz3QPAoVOaagReDddjULy06o1Q9vn929CTo05YMDQU44kVBzXDsTs8IiC5vJpnU")))
-
-if __name__ == '__main__':
-    draw_pupu()
+l=''
+for d in '00d80c;0b<0a>0a>540a=2<0aJ0bJ0S99J0Q;;G0N><F0N>;E0HI583;0DW0AY0?]0>_0=a0<b0;d0;e0:>4@4D0:;453:353B0:;2927392A0;92:272:3@0;92;262;2@0;92;262;2@09;2;262;2B08<2:272;2D07>2928383E06A7<8G05p05p05p05p06n07l08k09i0:f0=a0AX00':
+ if d=='0':
+  print (l);f=0;l=''
+ else:
+  l+=("*"if f else" ")*(ord(d)-49);f=~f
